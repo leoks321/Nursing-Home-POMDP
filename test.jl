17a523abc463e12d686adcb3830b6d2ae83165eb
leoks321 <55061112+leoks321@users.noreply.github.com>
2020-12-09 22:32:30 -0500

    Delete test.jl

--- test.jl
+++
@@ -1,8 +0,0 @@
-using POMDPs
-using Distributions: Normal, Uniform, DiscreteUniform, Multinomial, Hypergeometric, Binomial
-using Random
-using LinearAlgebra
-using Statistics
-using Plots
-import POMDPs: initialstate, gen, actions, discount, isterminal
-Random.seed!(1);
